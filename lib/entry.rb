a7b1de9f5ab6aed9a6f5448fca0487f37a25a7ab
Eugene Yak <geneayak@gmail.com>
2025-11-15 20:39:09 +0300

    add updates collecting

--- lib/entry.rb
+++ lib/entry.rb
@@ -14,6 +14,8 @@ class Entry
 
     update => update_id: update_id
 
+    DB.from(:updates).insert(update_id: update_id, payload:  Sequel.pg_json_wrap(update))
+
     Sentry.configure_scope do |scope|
       scope.set_tag("update_id", update_id)
       scope.set_context("update", update)
@@ -53,7 +55,7 @@ class Entry
         Sentry.start_transaction(op: text, description: "Update #{update_id}")
 
       in update_id: update_id, message: { from: user } if dialogue = dialogue_with(user)
-        Sentry.start_transaction(description: "Update #{update_id}", trace_id: @dialogue[:trace_id])
+        Sentry.start_transaction(description: "Update #{update_id}", trace_id: dialogue[:trace_id])
 
       in update_id: update_id
         Sentry.start_transaction(description: "Update #{update_id}")
@@ -65,6 +67,6 @@ class Entry
   end
 
   def dialogue_with(user)
-    @dialogue ||= DB.from(:dialogues).first(user_id: user[:id])
+    DB.from(:dialogues).first(user_id: user[:id])
   end
 end
